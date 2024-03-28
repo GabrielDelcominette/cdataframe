7e15e35aab8b7b0444c41f406b2669492b40e2e3
EGromaire <etienne.gromaire77@orange.fr>
2024-03-28 15:45:35 +0100

    cdataframes

--- cdataframe.c
+++ cdataframe.c
@@ -3,3 +3,20 @@
 //
 
 #include "cdataframe.h"
+<<<<<<< Updated upstream
+=======
+#include <stdlib.h>
+
+CDATAFRAME* create_cdataframe(){
+    CDATAFRAME * dataframe;
+    dataframe = (CDATAFRAME*) malloc(sizeof(CDATAFRAME));
+    dataframe->TP = 10;
+    dataframe->TL = 0;
+    dataframe->columns = NULL;
+    return dataframe;
+}
+
+void read_cdataframe_user(CDATAFRAME * cdataframe){
+    
+}
+>>>>>>> Stashed changes

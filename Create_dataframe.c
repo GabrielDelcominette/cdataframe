9adb78fd146552446c10bc9b9ebadfbbfa08568d
GabrielDelcominette <150939579+GabrielDelcominette@users.noreply.github.com>
2024-03-28 15:23:52 +0100

    fonction insert_value

--- Create_dataframe.c
+++ Create_dataframe.c
@@ -5,8 +5,27 @@ COLUMN* create_column(char* title){
     COLUMN * column;
     column = (COLUMN*) malloc(sizeof(COLUMN));
     column->title = title;
-    column->TP = 256;
+    column->TP = REALOC_SIZE;
     column->TL = 0;
     column->data = NULL;
     return column;
+}
+
+int insert_value(COLUMN* col, int value){
+    if (col->data == NULL){
+        col->data = (int*) malloc(256* sizeof(int));
+    }
+    if (col->data == NULL){
+        return 0;
+    }
+    if (col->TL == col->TP){
+        col->TP += REALOC_SIZE;
+        realloc(col->data, col->TP * sizeof(int));
+    }
+    if (col->TL < col->TP){
+        col->data[col->TL-1] = value;
+        col->TL+=1;
+        return 1;
+    }
+    return 0;
 }
--- Create_dataframe.h
+++ Create_dataframe.h
@@ -1,6 +1,8 @@
 #ifndef CDATAFRAME_CREATE_DATAFRAME_H
 #define CDATAFRAME_CREATE_DATAFRAME_H
 
+#define REALOC_SIZE 256;
+
 typedef struct {
     char * title;
     int TP;
@@ -9,5 +11,14 @@ typedef struct {
 }COLUMN;
 
 COLUMN* create_column(char* title);
+
+/**
+* @brief : Add a new value to a column
+* @param1 : Pointer to a column
+* @param2 : The value to be added
+* @return : 1 if the value is added 0 otherwise
+*/
+int insert_value(COLUMN* col, int value);
+
 
 #endif //CDATAFRAME_CREATE_DATAFRAME_H
